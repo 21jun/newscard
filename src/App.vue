e33875f7dca27854eed2e55869a764d9c1ebdeaa
21jun <21jun7654@gmail.com>
2018-07-31 23:00:56 +0900

    add cards

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,91 @@
 <template>
   <div id="app">
     <search-bar v-on:changekeyword=sendkeyword></search-bar>
-    <news-card v-bind:items="tophun"></news-card>
-    <naver-list v-bind:tophun="tophun" ></naver-list>
+    <news-card v-bind:items="topten"></news-card>
+    <naver-list v-bind:tophun="tophun"></naver-list>
 
   </div>
 </template>
 
 <script>
-import cleanStr from './add_js/cleanString'
-import child from './components/Child'
-import NaverList from './components/NaverList'
-import axios from 'axios'
-import SearchBar from './components/SearchBar'
-import NewsCard from  './components/NewsCard'
+  import cleanStr from './add_js/cleanString'
+  import child from './components/Child'
+  import NaverList from './components/NaverList'
+  import axios from 'axios'
+  import SearchBar from './components/SearchBar'
+  import NewsCard from './components/NewsCard'
 
-let clientId = "K99rkGyg42Jf0FBSTQgO";
-let clientSecret = "M0BbIqUL5F";
+  let clientId = "K99rkGyg42Jf0FBSTQgO";
+  let clientSecret = "M0BbIqUL5F";
 
-export default {
-  name: 'App',
-  components: {
-    child,
-    NaverList,
-    SearchBar,
-    NewsCard,
-  },
-  data() {
-    return {
-      title: [],
-      link: [],
-      tophun: [],
-      parentKeyWord: "세종대"
-    }
-  },
-  mounted() {
-    this.search();
-  },
-  methods: {
-    search: function () {
-      axios({
-        method: "get",
-        url: "/naversearch",
-        params: {
-          query: this.parentKeyWord,
-          display: "100"
-        },
-        crossDomain: true,
-        headers: {
-          "X-Naver-Client-Id": clientId,
-          "X-Naver-Client-Secret": clientSecret
-        }
-      }).then((result) => {
-        //console.log(result.data.items[50]);
-        let news = result.data.items;
-        for (let i in news) {
-          var tmp = {
-            title: cleanStr(news[i].title, "title"),
-            link: (news[i].originallink)
-          };
-          this.tophun.push(tmp);
-        }
-      })
+  export default {
+    name: 'App',
+    components: {
+      child,
+      NaverList,
+      SearchBar,
+      NewsCard,
     },
-    sendkeyword:function (val) {
-      this.tophun=[];
-      this.parentKeyWord = val;
-      this.search();
-    }
-  },
+    data() {
+      return {
+        title: [],
+        link: [],
+        tophun: [],
+        topten: [],
+        parentKeyWord: "세종대",
 
-}
+      }
+    },
+    mounted() {
+      this.search();
+    },
+    methods: {
+      search: function () {
+        axios({
+          method: "get",
+          url: "/naversearch",
+          params: {
+            query: this.parentKeyWord,
+            display: "100"
+          },
+          crossDomain: true,
+          headers: {
+            "X-Naver-Client-Id": clientId,
+            "X-Naver-Client-Secret": clientSecret
+          }
+        }).then((result) => {
+          //console.log(result.data.items[50]);
+          let news = result.data.items;
+          for (let i in news) {
+            var tmp = {
+              title: cleanStr(news[i].title, "title"),
+              link: (news[i].originallink)
+            };
+            this.tophun.push(tmp);
+          }
+          for (let i = 0; i < 10; i++) {
+            var tmp = {
+              title: cleanStr(news[i].title, "title"),
+              link: (news[i].originallink)
+            };
+            this.topten.push(tmp);
+          }
+        })
+      },
+      sendkeyword: function (val) {
+        this.tophun = [];
+        this.topten = [];
+        this.parentKeyWord = val;
+        this.search();
+      }
+    },
+
+  }
 </script>
 
 <style>
-#app {
-  margin: 10px auto;
-  width: 1000px;
-}
+  #app {
+    margin: 10px auto;
+    width: 1000px;
+  }
 </style>
--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,55 +1,66 @@
 <template>
-  <v-layout row>
-    <v-flex xs12 sm3 offset-sm1 v-for="i in 3">
-      <v-card>
-        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
-        <v-card-media
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="200px"
-        >
-        </v-card-media>
-
-        <v-card-title primary-title class="link">
-          <div>
-            <div class="headline">{{items[i].link}}</div>
-            <span class="grey--text">{{items[i].title}}</span>
-          </div>
-        </v-card-title>
-
-        <v-card-actions>
-          <v-btn flat>Share</v-btn>
-          <v-btn flat color="purple">Explore</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="show = !show">
-            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-slide-y-transition>
-          <v-card-text v-show="show">
-
-          </v-card-text>
-        </v-slide-y-transition>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container fluid grid-list-md>
+    <v-data-iterator
+      :items="items"
+      :rows-per-page-items="rowsPerPageItems"
+      :pagination.sync="pagination"
+      content-tag="v-layout"
+      row
+      wrap
+    >
+      <v-flex
+        slot="item"
+        slot-scope="props"
+        xs12
+        sm6
+        md4
+        lg2
+      >
+        <v-card>
+          <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+          <v-card-media
+            class="white--text"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            @click="linking(props.item.link)"
+          >
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">{{ props.item.title }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-media>
+          <v-card-title>
+            <div>
+              <span class="grey--text"></span><br>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-data-iterator>
+  </v-container>
 </template>
 
 <script>
   export default {
     name: "NewsCard",
-    props:['items'],
-    data() {
-      show:false
+    props: ['items'],
+    data: () => ({
+      rowsPerPageItems: [5, 10, 15],
+      pagination: {
+        rowsPerPage: 5
+      },
+    }),
+    methods:{
+      linking:function (link) {
+        window.open(link);
+      }
     }
   }
 </script>
 
 <style scoped>
-  .link{
-    overflow: auto;
-  }
-  .headline{
 
-  }
 </style>
